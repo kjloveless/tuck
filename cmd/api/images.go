8e00a319bde71c63eb384c2cd36ca66f0a1ebe36
Kyle Loveless <kjloveless321@gmail.com>
2026-07-13 23:21:20 -0500

    query string params

--- cmd/api/images.go
+++ cmd/api/images.go
@@ -190,8 +190,9 @@ func (app *application) listImagesHandler(w http.ResponseWriter, r *http.Request
 	input.Filters.PageSize = app.readInt(qs, "page_size", 20, v)
 
 	input.Filters.Sort = app.readString(qs, "sort", "id")
+	input.Filters.SortSafelist = []string{"id", "location", "year", "-id", "-location", "-year"}
 
-	if !v.Valid() {
+	if data.ValidateFilters(v, input.Filters); !v.Valid() {
 		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
